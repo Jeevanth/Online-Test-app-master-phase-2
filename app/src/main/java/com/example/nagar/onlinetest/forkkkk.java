a848dfceef031ab234e3f2af97bb946183bfd5b7
Nagarjun-CS <nagarjuncs007@gmail.com>
2020-06-11 12:18:14 +0530

    nagarjun  2222

--- app/src/main/java/com/example/nagar/onlinetest/forkkkk.java
+++ app/src/main/java/com/example/nagar/onlinetest/forkkkk.java
@@ -3,11 +3,11 @@ package com.example.nagar.onlinetest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class tesssssttttttttttt extends AppCompatActivity {
+public class forkkkk extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tesssssttttttttttt);
+        setContentView(R.layout.activity_forkkkk);
     }
 }
